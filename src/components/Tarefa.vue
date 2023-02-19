32336f968f092d907986d06bcbde03b11b6f592d
Mateus Maranhão <feradourada@gmail.com>
2023-02-19 18:35:20 -0300

    implement method removerTarefa()

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -3,18 +3,8 @@
             <div class="px-2 py-2">
                 <div class="font-bold text-xl mb-2">{{ d.tarefa }}</div>
             </div>
-            <div class="px-2 py-2">
-                <div class="text-sm mb-2">
-                    <span style="font-weight: bold; color: #000;">
-                        Data: 
-                    </span> 
-                    <span style="font-style: normal; color: gray;" class="text-sm mb-2">
-                        17/01/2023
-                    </span>
-                </div>
-            </div>
             <div class="px-2 pt-2 pb-1">
-                <button class="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded">
+                <button @click="removerTarefa(d.id)" class="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded">
                     <i class="fa-solid fa-trash"></i>
                 </button>
                 <button class="bg-transparent hover:bg-cyan-500 text-cyan-700 font-semibold hover:text-white py-2 px-4 border border-cyan-500 hover:border-transparent rounded">
@@ -37,6 +27,17 @@
         created() {
             this.getDadosApi('http://localhost:3000/tarefas')
         },
+
+        methods: {
+            async removerTarefa(id) {
+
+                await fetch (`http://localhost:3000/tarefas/${id}`, {
+                    method: 'DELETE'
+                })
+
+                window.location.reload();
+            }
+        }
     }
 </script>
 
